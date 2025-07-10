d6609b8b3db3bb97d6103536c0d8691912b93103
BLvckbutter0 <lowoski3@gmail.com>
2025-07-10 18:48:57 +0200

    Add files via upload

---
+++ emp.py
@@ -0,0 +1,13 @@
+emp=open("emp.txt", "r")
+
+print(emp.readline())
+print(emp.readline())
+emp.close()
+
+#jit=open("emp.txt","r")
+#for employee in jit.readlines():
+    #print(employee)
+
+#print(jit.readline()[1])
+
+#jit.close()
